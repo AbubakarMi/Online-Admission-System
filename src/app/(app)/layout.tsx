d2dc9f191b9edb119ea505d0947e4a49c3c104a3
Abubakar Mi <abubakarmi131@gmail.com>
2025-09-28 13:47:37 +0000

    any impromenet

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -137,7 +137,7 @@ export default function AppLayout({
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Logo className="h-6 w-6 text-primary" />
-              <span className="">Adustech Online System</span>
+              <span>Adustech Online System</span>
             </Link>
             <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
